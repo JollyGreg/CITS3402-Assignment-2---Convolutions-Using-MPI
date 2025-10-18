b5ee4ecc98ffbc00c1d4c7114d7a8924439d0f59
JollyGreg <liambush05@gmail.com>
2025-10-18 15:33:09 +0800

    added print flag -p

--- main.c
+++ main.c
@@ -104,6 +104,7 @@ int main(int argc, char *argv[]) {
     char *output_file = NULL;
 
     int H = 0, W = 0, kH = 0, kW = 0, sH = 0, sW = 0;
+    int print = 1;
 
     // Parse arguments
 	for (int i = 1; i < argc; i++) {
@@ -116,6 +117,7 @@ int main(int argc, char *argv[]) {
         else if (!strcmp(argv[i], "-o")) output_file = argv[++i];
         else if (!strcmp(argv[i], "-sH")) sH = (int)atoll(argv[++i]);
         else if (!strcmp(argv[i], "-sW")) sW = (int)atoll(argv[++i]);
+        else if (!strcmp(argv[i], "-p")) print = (int)atoll(argv[++i]);
         else {
             printf("Unknown argument: %s\n", argv[i]);
             exit(EXIT_FAILURE);
@@ -172,7 +174,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Print the feature and kernel matrix (rank 0 only)
-    if (rank == 0) {
+    if (rank == 0 && print == 1) {
         printf("Features (f)\n");
         print_matrix(f, H, W);
         printf("Kernels (g)\n");
@@ -198,7 +200,7 @@ int main(int argc, char *argv[]) {
     double WALL_time = WALL_end - WALL_begin;
 
     // Save and print output from rank 0 only
-    if (rank == 0) {
+    if (rank == 0 && print == 1) {
         if (output_file) save_matrix(output_file, o, o_H, o_W);
         printf("Output (o)\n");
         print_matrix(o, o_H, o_W);
